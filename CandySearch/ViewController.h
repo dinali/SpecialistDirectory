7aad7f089f742bf3898be1c327763dca072a5aee
Nicolas <nmartin413@gmail.com>
2012-07-05 05:37:36 -0500

    Initial Commit

---
+++ CandySearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CandySearch
+//
+//  Created by Nicolas Martin on 7/5/12.
+//  Copyright (c) 2012 University of Illinois at Urbana-Champaign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
